66d93ff123b848705a5f2890f90490cdb997c500
John Adeba <adebajohn@gmail.com>
2021-02-06 14:45:44 +0100

    3 per row

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import AddPerson from './addPerson.jsx';
 import QueueRow from './queueRow.jsx';
 
@@ -6,27 +6,38 @@ export default function App() {
     const [queues, setQueues] = useState([{ name: 'John Doe', tasks: ['Groceries', 'Take out the trash'] }]);
     const [rows, setRows] = useState([]);
 
-    useMemo(() => {      
-        let rows =[[]];
-        for (let i = 0; i < queues.length; i++) {
-           
-                if( (i+1) % 4 == 0 )
-                    rows.push([queues[i]]);
-                else
-                   rows[rows.length-1].push(queues[i]);       
-        }
+    useMemo(() => {
+        setRowData();
        
-        setRows(rows);
-    }, [queues]); 
+        return () => remove.setRowData();
+    }, [queues]);
 
+    function setQueuesData(data){
+        setQueues(data);
+    }
+
+    function setRowData(){
+        
+        const newRows = [];
+        let currentRow = []
+        for (let i = 0; i < queues.length; i++) {
+            if (currentRow.length === 3) {
+                newRows.push(currentRow);
+                currentRow = [];
+            }
+            currentRow.push(queues[i]);
+        }
+        if (currentRow.length > 0) newRows.push(currentRow);
+        setRows(newRows);
+    }
     console.log('render: App');
 
     return (
         <div>
             <div className="title">Tasks</div>
-            <AddPerson currentPeople={queues} setPeople={setQueues} />
+            <AddPerson currentPeople={queues} setPeople={setQueuesData} />
             <div className="queues-wrapper">
-                {rows.map((row, idx) => <QueueRow key={`${idx}`} idx={idx} row={row} currentPeople={queues} setPeople={setQueues} />)}
+                {rows.map((row, idx) => <QueueRow key={`${idx}`} idx={idx} row={row} currentPeople={queues} setPeople={setQueuesData} />)}
             </div>
         </div>
     );
